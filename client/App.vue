861eeeb83a6603a996df3baaeb3f12d4020883dd
Christian Meredith <ceigey@users.noreply.github.com>
2019-07-28 15:49:13 +0930

    vue init

---
+++ client/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <header>
+      <h1>Vue Stack Test</h1>
+    </header>
+    <div id="content">
+      <p>
+        {{ initMessage }}
+      </p>
+      <p>
+        Testing, one, two, three, do you read me?
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ts-check
+export default {
+  data() {
+    return {
+      initMessage: 'Hello, World!'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
