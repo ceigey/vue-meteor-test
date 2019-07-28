ac02b0cef1f14e18107c75fc35f077ca72f2167f
Christian Meredith <ceigey@users.noreply.github.com>
2019-07-28 16:35:53 +0930

    sass init

--- client/App.vue
+++ client/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header>
+    <header class="test-header">
       <h1>Vue Stack Test</h1>
     </header>
     <div id="content">
@@ -9,6 +9,15 @@
       </p>
       <p>
         Testing, one, two, three, do you read me?
+      </p>
+      <p>
+        Test button:
+        <button
+          class="test-button"
+          @click="increment"
+        >
+          Count: {{ count }}
+        </button>
       </p>
     </div>
   </div>
@@ -19,12 +28,27 @@
 export default {
   data() {
     return {
-      initMessage: 'Hello, World!'
+      initMessage: 'Hello, World!',
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+  @import '../imports/scss/button/filled';
+  @import '../imports/scss/header/test';
 
+  .test-button {
+    @include button-filled(#008844);
+  }
+
+  .test-header {
+    @include header-test(#6655dd, true);
+  }
 </style>
